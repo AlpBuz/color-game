34c552c1ac25688f9910d80149d4db49fd3eeae7
AlpBuz <alpbuz@buffalo.edu>
2024-05-11 15:23:59 -0400

    Fixed issue with backend and frontend connection. Backend is also implemented

---
+++ flask-server/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from helpers import nextColor, nextWord
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+
+@app.route("/confirmAnswer", methods=["POST"])
+def ConfirmAnswer():
+    response_data = {}
+    data = request.form
+    color = data["color"].lower()
+    userAnswer = data["userAnswer"].lower()
+
+
+
+    if(color == userAnswer): #check if users answer is correct
+        response_data["valid"] = True
+        response_data["newColor"] = nextColor()
+        response_data["newWord"] = nextWord(response_data["newColor"])
+        return jsonify(response_data)
+    else:
+        response_data["valid"] = False
+        return jsonify(response_data)
+        
+
+
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
